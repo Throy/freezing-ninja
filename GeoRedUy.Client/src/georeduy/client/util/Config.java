cd4f0e0514e1fa6c8b1e6c153bf05c50cf656279
Agustin <agustin@lavabit.com>
2012-10-07 17:41:59 -0200

    Cambio de ip para conectarse al host

--- GeoRedUy.Client/src/georeduy/client/util/Config.java
+++ GeoRedUy.Client/src/georeduy/client/util/Config.java
@@ -4,7 +4,7 @@ package georeduy.client.util;
  * Helper class providing constants common to other classes in the app.
  */
 public final class Config {
-	static final String SERVER_URL = "http://10.190.46.101/GeoRedUy.Server";
+	static final String SERVER_URL = "http://10.0.2.2/GeoRedUy.Server";
 	
 	static final int SERVER_PORT = 8080;
 }

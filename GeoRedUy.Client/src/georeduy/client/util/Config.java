8b19db1e9dbee5663e20e374d5b555bbed8d15bf
Agustin <agustin@lavabit.com>
2012-10-18 17:59:28 -0200

    Cambios minimos

--- GeoRedUy.Client/src/georeduy/client/util/Config.java
+++ GeoRedUy.Client/src/georeduy/client/util/Config.java
@@ -5,8 +5,4 @@ package georeduy.client.util;
  */
 public final class Config {
 	public static final String SERVER_URL = "http://10.0.2.2:8080/GeoRedUy.Server";
-	
-	public static final String GCM_SERVER_URL = "http://10.0.2.2:8080/GeoRedUy.Server";
-	
-	public static final int SERVER_PORT = 8080;
 }

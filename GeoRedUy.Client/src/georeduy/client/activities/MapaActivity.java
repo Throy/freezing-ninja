7ebebfb61e0cf7a7391b321dbf9a7592948b9275
NaBUru38 <naburu38@yahoo.com>
2012-10-20 21:22:52 -0200

    [Client] Paquete nuevo "maps" para el código de los overlays; cliquear en un sitio ahora mustra un diálogo con aceptar y cancelar

--- GeoRedUy.Client/src/georeduy/client/activities/MapaActivity.java
+++ GeoRedUy.Client/src/georeduy/client/activities/MapaActivity.java
@@ -3,6 +3,8 @@ package georeduy.client.activities;
 import java.util.List;
 
 import georeduy.client.activities.R;
+import georeduy.client.maps.CustomItemizedOverlay;
+
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
@@ -15,29 +17,36 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 public class MapaActivity extends MapActivity {
+	// mapa
 	private MapView mapView;
+	
+	// cordenadas de Atenas
 	private static final int latitudeE6 = 37985339;
 	private static final int longitudeE6 = 23716735;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	
-		setContentView(georeduy.client.activities.R.layout.activity_main);
-		mapView = (MapView) findViewById(georeduy.client.activities.R.id.map_view);
-		mapView.setBuiltInZoomControls(true);		
+		setContentView (georeduy.client.activities.R.layout.activity_main);
+		
+		// obtener mapa
+		mapView = (MapView) findViewById (georeduy.client.activities.R.id.map_view);
+		mapView.setBuiltInZoomControls (true);
+
+		// inicializar overlay, con íconos carrito
 		List<Overlay> mapOverlays = mapView.getOverlays();
-		Drawable drawable = this.getResources().getDrawable(R.drawable.cart);
-		CustomItemizedOverlay itemizedOverlay = new CustomItemizedOverlay(
-		        drawable, this);
+		Drawable drawable = this.getResources().getDrawable (R.drawable.cart);
+		CustomItemizedOverlay itemizedOverlay = new CustomItemizedOverlay (drawable, this);
 
-		GeoPoint point = new GeoPoint(latitudeE6, longitudeE6);
-		OverlayItem overlayitem = new OverlayItem(point, "Hola",
-		        "Estoy en atenas!");
+		// agregar ítems al overlay
+		GeoPoint point = new GeoPoint (latitudeE6, longitudeE6);
+		OverlayItem overlayitem = new OverlayItem(point, "Hola", "Estoy en atenas!");
+		itemizedOverlay.addOverlay (overlayitem);
+		
+		// agregar overlay al mapa
+		mapOverlays.add (itemizedOverlay);
 
-		itemizedOverlay.addOverlay(overlayitem);
-		mapOverlays.add(itemizedOverlay);
-
+		// encuadrar mapa en Atenas
 		MapController mapController = mapView.getController();
 
 		mapController.animateTo(point);
---
+++ GeoRedUy.Client/src/georeduy/client/maps/CustomItemizedOverlay.java
@@ -0,0 +1,78 @@
+package georeduy.client.maps;
+
+import georeduy.client.activities.R;
+import georeduy.client.util.CommonUtilities;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.widget.TextView;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class CustomItemizedOverlay extends ItemizedOverlay<OverlayItem> {
+
+	private ArrayList<OverlayItem> mapOverlays = new ArrayList<OverlayItem>();
+
+	private Context context;
+
+	public CustomItemizedOverlay (Drawable defaultMarker) {
+		super(boundCenterBottom (defaultMarker));
+	}
+
+	public CustomItemizedOverlay (Drawable defaultMarker, Context context) {
+		this(defaultMarker);
+		this.context = context;
+	}
+
+	@Override
+	protected OverlayItem createItem (int i) {
+		return mapOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mapOverlays.size();
+	}
+
+	// cliquear en el botón -> mostrar cuadro
+	
+	@Override
+	protected boolean onTap (int index) {
+		OverlayItem item = mapOverlays.get(index);
+		
+		AlertDialog dialog = new AlertDialog.Builder (context).create ();
+		dialog.setTitle (item.getTitle());
+		dialog.setMessage (item.getSnippet());
+		
+		dialog.setButton (DialogInterface.BUTTON_POSITIVE, "Aceptar", new DialogInterface.OnClickListener() {
+			public void onClick (DialogInterface dialog, int which) {
+				CommonUtilities.showAlertMessage (context, "Confirmación", "Agregaste la visita.");
+				dialog.cancel();
+			}
+		});
+		
+		dialog.setButton (DialogInterface.BUTTON_NEGATIVE, "Cancelar", new DialogInterface.OnClickListener() {
+			public void onClick (DialogInterface dialog, int which) {
+				CommonUtilities.showAlertMessage (context, "Cancelación", "Descartaste la visita.");
+				dialog.cancel();
+			}
+		});
+		
+		dialog.show();
+
+        //CommonUtilities.showAlertMessage (context, item.getTitle(), item.getSnippet());
+		
+		return true;
+	}
+
+	public void addOverlay (OverlayItem overlay) {
+		mapOverlays.add (overlay);
+		this.populate();
+	}
+
+}

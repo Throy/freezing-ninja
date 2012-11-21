3db89d2b2c83df6d7319240a86660837864866f3
mics <micalb@gmail.com>
2012-11-21 01:19:11 -0200

    cambio en la entidad retailer (test cliente git)

--- GeoRedUy.Server/src/georeduy/server/logic/model/Retailer.java
+++ GeoRedUy.Server/src/georeduy/server/logic/model/Retailer.java
@@ -17,7 +17,7 @@ public class Retailer {
     @Property
     private String description;
     @Property
-    private String imageUrl;
+    private byte[] image;
     @Property
     private String userId;
     @Property
@@ -49,12 +49,12 @@ public class Retailer {
 		this.description = description;
 	}
 	
-	public String getImageUrl() {
-		return imageUrl;
+	public byte[] getImage() {
+		return image;
 	}
 
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
+	public void setImage(byte image) {
+		this.image = image;
 	}
 
 	public User getUser() {
